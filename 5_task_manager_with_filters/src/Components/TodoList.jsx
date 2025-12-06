fde3fe41290bb183d74dc82019bce167ca8a54bb
amit39 <amitsonagra01@gmail.com>
2025-12-06 15:52:48 +0000

    fixed list container

--- 5_task_manager_with_filters/src/Components/TodoList.jsx
+++ 5_task_manager_with_filters/src/Components/TodoList.jsx
@@ -8,92 +8,113 @@ export function TodoList({
   filter,
 }) {
   return (
-    <>
+    <div
+      style={{
+        width: "700px",
+        backgroundColor: "#1a1a1a",
+        padding: "2rem",
+        borderRadius: "8px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "1rem",
+        height: "400px",
+      }}
+    >
       <ul
         style={{
-          width: "700px",
+          listStyle: "none",
+          margin: 0,
+          padding: 0,
+          flex: "1",
+          overflowY: "auto",
           display: "flex",
-          padding: "2rem 2rem",
-          borderRadius: "8px",
           flexDirection: "column",
           gap: "1rem",
-          backgroundColor: "#1a1a1a",
+
+          scrollbarGutter: "stable",
+
+          scrollbarWidth: "thin",
+          scrollbarColor: "#444 #1a1a1a",
         }}
       >
-        {todos.map((todo) => (
-          <li
-            style={{
-              backgroundColor: "#212121",
-              borderRadius: "8px",
-              paddingInline: "16px",
-              paddingBlock: "8px",
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-            key={todo.id}
-          >
-            <span
+        {todos.length > 0 &&
+          todos.map((todo) => (
+            <li
+              key={todo.id}
               style={{
-                cursor: "pointer",
-                textDecoration: todo.completed ? "line-through" : "none",
+                backgroundColor: "#212121",
+                borderRadius: "8px",
+                paddingInline: "16px",
+                paddingBlock: "8px",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
               }}
-              onClick={() => onCompletion(todo.id)}
             >
-              {todo.task}
-            </span>
-            <button className="delBtn" onClick={() => onDelete(todo.id)}>
-              Delete
-            </button>
-          </li>
-        ))}
+              <span
+                style={{
+                  cursor: "pointer",
+                  textDecoration: todo.completed ? "line-through" : "none",
+                }}
+                onClick={() => onCompletion(todo.id)}
+              >
+                {todo.task}
+              </span>
+
+              <button className="delBtn" onClick={() => onDelete(todo.id)}>
+                Delete
+              </button>
+            </li>
+          ))}
 
         {todos.length === 0 && totalTasks > 0 && (
           <li
             style={{
-              textAlign: "center",
               padding: "2rem",
-              color: "#888",
-              listStyle:"none"
+              textAlign: "center",
+              color: "#aaa",
             }}
           >
             No {filter} tasks found!
           </li>
         )}
 
-        <div>
-          {totalTasks > 0 ? (
-            <div>
-              <div
-                style={{
-                  width: "100%",
-                  height: "1px",
-                  backgroundColor: "#212121",
-                  marginBottom: "1rem",
-                  marginTop: "0.5rem",
-                  borderRadius: "999px",
-                }}
-              ></div>
-              <div style={{ display: "flex", justifyContent: "space-between" }}>
-                <p>
-                  <span style={{ fontWeight: 500 }}>Total Tasks:</span>{" "}
-                  {totalTasks}
-                </p>
-                <p>
-                  <span style={{ fontWeight: 500 }}>Completed Tasks:</span>{" "}
-                  {completedTask}
-                </p>
-                <p>
-                  <span style={{ fontWeight: 500 }}>Remaining Tasks:</span>{" "}
-                  {remainingTasks}
-                </p>
-              </div>
-            </div>
-          ) : (
-            <span>No Task Added Yet!</span>
-          )}
-        </div>
+        {totalTasks === 0 && (
+          <li
+            style={{
+              padding: "2rem",
+              textAlign: "center",
+              color: "#aaa",
+            }}
+          >
+            No Task Added Yet!
+          </li>
+        )}
       </ul>
-    </>
+
+      <div>
+        <div
+          style={{
+            width: "100%",
+            height: "1px",
+            backgroundColor: "#212121",
+            marginBottom: "1rem",
+            borderRadius: "999px",
+          }}
+        />
+
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <p>
+            <strong>Total Tasks:</strong> {totalTasks}
+          </p>
+          <p>
+            <strong>Completed Tasks:</strong> {completedTask}
+          </p>
+          <p>
+            <strong>Remaining Tasks:</strong> {remainingTasks}
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
